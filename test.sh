f33d2cc5ec298eac01d60f0f00670201ded02c7b
priya001 <priya001yadav1@gmail.com>
2023-03-16 01:31:47 +0530

    done

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# SheBang Notation tells the interpretor what the code and based on that it will understand and compile/run the script 
+# You can have only one SheBang Notation per script and should always be the first file or start of the script 
+
+# Apart from the shebang notation, anything that starts with `#` is referred as a comment and is not executed by the script
+# Comments are just for hte human readable purpose only.
+
+echo Hello World
+
+echo Line1 
+echo okkndklvnskldcs
+dbdkdklbmdlfvmdslvs
